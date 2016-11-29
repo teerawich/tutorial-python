28779827ca1f500f11324860f20a75f6833872a3
Teerawich Phasitthanakamol <p.teerawich@hotmail.com>
2016-11-29 23:59:31 +0700

    Add test variable

--- tutorial-01/tutorial-01.py
+++ tutorial-01/tutorial-01.py
@@ -7,3 +7,22 @@ name = "John"   # A string
 print (counter)
 print (miles)
 print (name)
+
+str = 'Hello World!'
+
+print (str)
+print (str[0])
+print (str[2:5])
+print (str[2:])
+print (str * 2)
+print (str + "TEST")
+
+list = [ 'abcd', 786, 2.23, 'john', 70.2 ]
+tinylist = [ 123, 'john' ]
+
+print (list)
+print (list[0])
+print (list[1:3])
+print (list[2:])
+print (tinylist * 2)
+print (list + tinylist)
