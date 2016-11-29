3cd86f2042de8459dbd5e9576a6721fe4059d770
Teerawich Phasitthanakamol <p.teerawich@hotmail.com>
2016-11-29 23:11:19 +0700

    Add function

--- tutorial-01/tutorial-01.py
+++ tutorial-01/tutorial-01.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
 
-input("\n\nPress the enter key to exit.")
-print ("Hello, Python!")
+counter = 100   # An integer assignment
+miles = 1000.0  # A floating point
+name = "John"   # A string
+
+print (counter)
+print (miles)
+print (name)
